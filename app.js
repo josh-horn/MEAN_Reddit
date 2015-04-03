f72029b9e4bc5a742846a9996c945237d65a96cd
josh-horn <joshhorn@udel.edu>
2015-04-03 16:31:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,8 @@ $scope.posts = [
   {title: 'post 4', upvotes: 9},
   {title: 'post 5', upvotes: 4}
 ];
+$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.title = '';
+};
